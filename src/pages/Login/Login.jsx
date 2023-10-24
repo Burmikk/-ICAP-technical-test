7e85ff56cd59f7ca0581516ec01b263ac2b60a7f
Burmikk <byrmik@gmail.com>
2023-10-24 21:40:06 +0300

    add yup validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,19 @@
-import { Field, Formik, Form } from "formik";
+import { Field, Formik, Form, ErrorMessage } from "formik";
 import scss from "./login.module.scss";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/auth/auth-operations";
+import { object, string } from "yup";
 
 const initialValues = {
     username: "",
     password: "",
 };
+
+const userSchema = object({
+    username: string().min(1).max(150).required("Name is required"),
+    password: string().min(1).max(128).required("Password is required"),
+});
+
 const Login = () => {
     const dispatch = useDispatch();
 
@@ -16,17 +23,19 @@ const Login = () => {
     };
     return (
         <>
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={userSchema}>
                 <Form>
                     <label>
                         Name
                         <Field type="text" name="username" />
+                        <ErrorMessage name="username" render={(msg) => <p>{msg}</p>} />
                     </label>
                     <label>
                         Password
                         <Field type="password" name="password" />
+                        <ErrorMessage name="password" render={(msg) => <p>{msg}</p>} />
                     </label>
-                    <button>LogIn</button>
+                    <button type="submit">LogIn</button>
                 </Form>
             </Formik>
         </>
