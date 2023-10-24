dc9481e56c253fdcd37caa822d06d1761ec469cc
Burmikk <byrmik@gmail.com>
2023-10-24 17:36:52 +0300

    create PageNotFound component

--- src/mainRoute.jsx
+++ src/mainRoute.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login/Login";
 import Table from "./pages/Table/Table";
 import PublicRoute from "./components/PublicRoute/PublicRoute";
 import PrivatRoute from "./components/PrivatRoute/PrivatRoute";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 const MainRoute = () => {
     return (
@@ -14,6 +15,7 @@ const MainRoute = () => {
                 <Route element={<PrivatRoute />}>
                     <Route path="/table" element={<Table />} />
                 </Route>
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </>
     );
---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import scss from "./pageNotFound.module.scss";
+const PageNotFound = () => {
+    return (
+        <>
+            <h2>404 Page Not Found </h2>
+        </>
+    );
+};
+export default PageNotFound;
